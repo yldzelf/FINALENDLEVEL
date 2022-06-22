df6fcb9a7a74fdaee5d963fa2bfc28b119391617
yldzelf <yldz0300@gmail.com>
2022-06-23 00:46:56 +0300

    Initial Commit

---
+++ ARTIKOLLLLL-main/Assets/scripts/Crawlerfollowsyou.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Crawlerfollowsyou : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform Player;
+
+    // public float distToPlayer = 5,0f;
+    // public float faceplayerFactor = 20f;
+    // public float attackRadius = 1,0f;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        enemy.SetDestination(Player.position);
+
+        // float distance = vector3.Distance(PlayerMove.PlayerPos, transform.position);
+        // if(distance > attackRadius) {stop();}
+        // else if (distance <= attackRadius) {Attack();}
+    }
+}
---
+++ ARTIKOLLLLL-main/Assets/scripts/lookatme.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookatme : MonoBehaviour
+{
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // CharacterController controller = Player.GetComponent<CharacterController>();
+        
+        Vector3 lookVector = Player.transform.position - transform.position;
+        lookVector.y = transform.position.y;
+        Quaternion rot = Quaternion.LookRotation(lookVector);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);
+    }
+}
